7d571a3bcbddf26a3be4a5ba3da1565d7577a5b1
miniusaco <80330292+miniusaco@users.noreply.github.com>
2022-04-19 13:56:51 +0900

    35.Pageの作成

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "いぬっこ",
-  image: "https://source.unsplash.com/BJaqPaH6AGQ",
-  email: "one@example.com",
-  phone: "111-1111-1111",
-  company: {
-    name: "いぬいぬ会社"
-  },
-  website: "http://mole.com"
-};
-
 export default function App() {
   return <Router />;
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,46 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearcInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `いぬっこ${val}`,
+    image: "https://source.unsplash.com/BJaqPaH6AGQ",
+    email: "one@example.com",
+    phone: "111-1111-1111",
+    company: {
+      name: "いぬいぬ会社"
+    },
+    website: "http://mole.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map(() => (
+          <UserCard key={users.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: colomn;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
